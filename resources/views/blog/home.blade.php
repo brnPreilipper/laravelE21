36d3f23ff458dd386d089b581e3893d2142cb9e6
Bruno_Preilipper <brnpreilipper09@gmail.com>
2024-02-05 19:28:47 -0300

    meu primeiro commit

---
+++ resources/views/blog/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1 class="text-orange">Seja bem-vindo</h1>
+    <button class="btn btn-chief">BOTÃO</button>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/app.js"></script>
+</body>
+</html>
